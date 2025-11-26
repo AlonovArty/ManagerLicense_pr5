2f7270a72957cfeb4f115a3b0b735561eb618a18
alexeys6969 <alexshashin.2023@gmail.com>
2025-11-26 15:25:13 +0500

    готова на 5

--- Server/Classes/Client.cs
+++ Server/Classes/Client.cs
@@ -14,7 +14,7 @@ namespace Server.Classes
         public Client()
         {
             Random random = new Random();
-            string Chars = "FDSAFASGfdsfGSDFGSDFGFDSF123456789888844411111";
+            string Chars = "FDSAFASGfdsfGSDFGSDFGFDSF12345678988884441111";
             this.Token = new string(Enumerable.Repeat(Chars, 15).Select(x => x[random.Next(Chars.Length)]).ToArray());
             DateConnect = DateTime.Now;
         }
